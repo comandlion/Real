b7077a02f7fc5a9f98c75b7c3ce463185f35ec04
comanddragon <ntsemancho@gmail.com>
2025-08-02 18:13:49 +0100

    testing django api

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App.tsx";
-import { AuthProvider } from './context/AuthProvider';
-import {UserProvider} from "./context/UserContext.tsx";
+import { AuthProvider } from '@/context/AuthProvider';
+import {UserProvider} from "@/context/UserContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
